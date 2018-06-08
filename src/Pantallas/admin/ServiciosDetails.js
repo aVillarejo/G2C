1a8096e75003cfb9013978b1daf528a7d5bd28ba
“a_villarejo19” <a.villarejo19@gmail.com>
2018-06-08 14:25:32 -0500

    Servicios

--- src/Pantallas/admin/ServiciosDetails.js
+++ src/Pantallas/admin/ServiciosDetails.js
@@ -1,25 +1,193 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  Platform,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+  Button
+} from "react-native";
+import { List, ListItem, SearchBar } from "react-native-elements";
+import ServerURL from "../../Config/ServerURL";
+import { NavigationActions } from "react-navigation";
 
 export default class ServiciosDetails extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      Id: "",
+      Nombre: "",
+      Descripcion: ""
+    };
   }
+
+  componentDidMount() {
+    // Received Student Details Sent From Previous Activity and Set Into State.
+    this.setState({
+      Id: this.props.navigation.state.params.obj.Id,
+      Nombre: this.props.navigation.state.params.obj.Nombre,
+      Descripcion: this.props.navigation.state.params.obj.Descripcion
+    });
+  }
+
+  static navigationOptions = { title: "Detalles Categoria" };
+
+  UpdateRecord = () => {
+    fetch(`${ServerURL}/servicios/actualizar.php`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        Id: this.state.Id,
+        Nombre: this.state.Nombre,
+        Descripcion: this.state.Descripcion
+      })
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        // Showing response message coming from server updating records.
+        Alert.alert(
+          "Aviso!",
+          responseJson,
+          [
+            {
+              text: "Aceptar",
+              onPress: () => this.props.navigation.navigate("ServiciosList")
+            }
+          ],
+          { cancelable: false }
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  DeleteRecord = () => {
+    fetch(`${ServerURL}/servicios/eliminar.php`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        Id: this.state.Id
+      })
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        // Showing response message coming from server after inserting records.
+        Alert.alert(
+          "Aviso!",
+          responseJson,
+          [
+            {
+              text: "Aceptar",
+              //onPress: () => this.props.navigation.navigate("UsersList")
+              onPress: () => this.props.navigation.navigate("CategoriasList")
+            }
+          ],
+          { cancelable: false }
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+    this.props.navigation.navigate("First");
+  };
 
   render() {
     return (
-      <View style={styles.Container}>
-        <Text>ServiciosDetails</Text>
+      <View style={styles.MainContainer}>
+        <TextInput
+          placeholder="Nombre"
+          value={this.state.Nombre}
+          onChangeText={TextInputValue =>
+            this.setState({ Nombre: TextInputValue })
+          }
+          underlineColorAndroid="transparent"
+          style={styles.TextInputStyleClass}
+        />
+        <TextInput
+          placeholder="Descripcion"
+          value={this.state.Descripcion}
+          onChangeText={TextInputValue =>
+            this.setState({ Descripcion: TextInputValue })
+          }
+          underlineColorAndroid="transparent"
+          style={styles.TextInputStyleClass}
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.4}
+          style={styles.TouchableOpacityStyle}
+          onPress={this.UpdateRecord}
+        >
+          <Text style={styles.TextStyle}>Actualizar Datos</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          activeOpacity={0.4}
+          style={styles.TouchableOpacityStyle}
+          onPress={this.DeleteRecord}
+        >
+          <Text style={styles.TextStyle}>Eliminar Categoria</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
-  Container: {
+  MainContainer: {
+    alignItems: "center",
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    paddingTop: 30,
+    backgroundColor: "#fff"
+  },
+
+  MainContainer_For_Show_StudentList_Activity: {
+    flex: 1,
+    paddingTop: Platform.OS == "ios" ? 20 : 0,
+    marginLeft: 5,
+    marginRight: 5
+  },
+
+  TextInputStyleClass: {
+    textAlign: "center",
+    width: "90%",
+    marginBottom: 7,
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#FF5722",
+    borderRadius: 5
+  },
+
+  TouchableOpacityStyle: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 5,
+    marginBottom: 7,
+    width: "90%",
+    backgroundColor: "#00BCD4"
+  },
+
+  TextStyle: {
+    color: "#fff",
+    textAlign: "center"
+  },
+
+  rowViewContainer: {
+    fontSize: 20,
+    paddingRight: 10,
+    paddingTop: 10,
+    paddingBottom: 10
   }
 });
--- src/Pantallas/admin/ServiciosList.js
+++ src/Pantallas/admin/ServiciosList.js
@@ -1,27 +1,122 @@
 import React, { Component } from "react";
 import {
-  StyleSheet,
-  Text,
   View,
-  FlatList,
+  Text,
   Button,
-  TouchableOpacity,
-  Alert
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity
 } from "react-native";
-import FlatListData from "./flatListData";
+import { List, ListItem, SearchBar } from "react-native-elements";
 import ServerURL from "../../Config/ServerURL";
 
-export default class ServiciosList extends Component {
+export default class CategoriasList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeRowKey: null,
       loading: false,
       data: [],
+      page: 1,
+      seed: 1,
       error: null,
-      refreshing: false
+      refreshing: false,
+      isLoading: true,
+      isLoading2: false
     };
   }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    //const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=2`;
+    const url = `${ServerURL}/servicios/consultar.php`;
+
+    this.setState({ loading: true });
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          //data: page === 1 ? res.results : [...this.state.data, ...res.results],
+          isLoading: false,
+          data: res ? res : [...this.state.data, ...res],
+          error: res.error || null,
+          loading: false,
+          refreshing: false,
+          isLoading2: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
+
+  handleRefresh = () => {
+    this.setState(
+      {
+        page: 1,
+        seed: this.state.seed + 1,
+        refreshing: true
+      },
+      () => {
+        this.makeRemoteRequest();
+      }
+    );
+  };
+
+  handleLoadMore = () => {
+    this.setState(
+      {
+        page: this.state.page + 1
+      },
+      () => {
+        this.makeRemoteRequest();
+      }
+    );
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "86%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+        }}
+      />
+    );
+  };
+
+  renderHeader = () => {
+    return (
+      <SearchBar
+        containerStyle={{ flexDirection: "row", flex: 1 }}
+        inputStyle={{ flex: 1 }}
+        placeholder="Buscar..."
+        lightTheme
+        round
+      />
+    );
+  };
+
+  renderFooter = () => {
+    if (!this.state.loading) return null;
+
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          borderTopWidth: 1,
+          borderColor: "#CED0CE"
+        }}
+      >
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
 
   static navigationOptions = {
     title: "Listado Servicios",
@@ -33,79 +128,69 @@ export default class ServiciosList extends Component {
       />
     )
   };
-  componentDidMount() {
-    this.makeRemoteRequest();
-  }
-
-  makeRemoteRequest = () => {
-    const url = `${ServerURL}/categorias/consultar.php`;
-
-    this.setState({ loading: true });
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          //data: page === 1 ? res.results : [...this.state.data, ...res.results],
-          data: res,
-          error: res.error || null,
-          loading: false,
-          refreshing: false
-        });
-      })
-      .catch(error => {
-        this.setState({ error, loading: false });
-      });
-  };
-
   render() {
-    return (
-      <View style={styles.Container}>
-        <FlatList
-          keyExtractor={item => item.Nombre}
-          data={this.state.data}
-          renderItem={({ item, index }) => {
-            return <FlatListItem item={item} index={index} />;
+    if (this.state.isLoading) {
+      return (
+        <View
+          style={{
+            flex: 1,
+            paddingTop: 20,
+            alignItems: "center",
+            justifyContent: "space-between"
           }}
-        />
-      </View>
-    );
-  }
-}
+        >
+          <View style={{ flexDirection: "row" }}>
+            <SearchBar
+              placeholder="Buscar..."
+              lightTheme
+              round
+              containerStyle={{ flexDirection: "row", flex: 1 }}
+              inputStyle={{ flex: 1 }}
+            />
+          </View>
 
-class FlatListItem extends Component {
-  _onpress = () => {
-    Alert.alert(
-      "Eliminar!",
-      "Realmente deseas eliminar el registro",
-      [
-        {
-          text: "si",
-          onPress: () => console.warn("OK Pressed")
-        },
-        {
-          text: "no",
-          onPress: () => console.warn("No Pressed")
-        }
-      ],
-      { cancelable: false }
-    );
-  };
-  render() {
-    return (
-      <TouchableOpacity onPress={this._onpress}>
-        <View style={{ flex: 1 }}>
-          <Text>Nombre: {this.props.item.Nombre}</Text>
-          <Text>Descripcion: {this.props.item.Descripcion}</Text>
+          <View style={{ flex: 1, paddingTop: 20, alignItems: "center" }}>
+            <Text>Cargando Servicios</Text>
+            <ActivityIndicator />
+          </View>
         </View>
-      </TouchableOpacity>
+      );
+    }
+    return (
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() =>
+                this.props.navigation.navigate("ServiciosDetails", {
+                  obj: item
+                })
+              }
+            >
+              <ListItem
+                roundAvatar
+                title={`${item.Nombre}`}
+                subtitle={`${item.Descripcion}`} // avatar={{
+                //   uri:
+                //     item.Tipo != 0
+                //       ? "https://cdn.icon-icons.com/icons2/157/PNG/256/admin_user_man_22187.png"
+                //       : "https://cdn3.iconfinder.com/data/icons/users-6/100/654853-user-men-2-128.png"
+                // }}
+                containerStyle={{ borderBottomWidth: 0 }}
+              />
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item.Nombre}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+          ListFooterComponent={this.renderFooter}
+          onRefresh={this.handleRefresh}
+          refreshing={this.state.refreshing}
+          onEndReached={this.handleLoadMore}
+          onEndReachedThreshold={50}
+        />
+      </List>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  Container: {
-    flex: 1,
-    justifyContent: "center"
-    //alignItems: "center"
-  }
-});
